73029569afd3236a52b12c6fc810926193702195
Nolan Nordlund <nolan@projectrebel.io>
2021-03-15 15:57:29 -0500

    Added a composable

--- src/index.js
+++ src/index.js
@@ -15,3 +15,6 @@ function install(app, options) {
 export default { install };
 export { TagInput } from "./components/tags/tags.js";
 export { PasswordComplexity } from "./components/passwords/passwords.js";
+
+
+export { useBooleanState } from './composables/state/useBooleanState';
